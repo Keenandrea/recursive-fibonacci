72881bc58634b12ed61ac49f3eb6f26f11c122c1
cvoges12 <38054771+cvoges12@users.noreply.github.com>
2019-02-26 09:26:15 -0600

    Updated types and added main func

--- ANDKA.hs
+++ ANDKA.hs
@@ -1,7 +1,8 @@
--- |haskell one-liner. zipWith makes a
--- |list that calculates like so: zipW
--- |ith (+)[1,2,3][3,2,1] output: [4,4
--- |,4]. this is a lazy algorithm. ask
--- |it for n Fibonacci with the line t
--- |ake n fibo. recursion of Fibonacci 
-fibo = 0:1:zipWith (+) fibo (tail fibo)
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+main = do
+  a <- (\y -> read y :: Int) <$> getLine
+  putStrLn . show $ fib a
